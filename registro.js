a8f6046047f694d683a4c9e5404567c20a35f4a9
Parker092 <53531818+Parker092@users.noreply.github.com>
2023-06-24 13:11:11 -0700

    Update registro.js

--- registro.js
+++ registro.js
@@ -1,7 +1,4 @@
-document.getElementById('myForm').addEventListener('submit', function(e) {
-    e.preventDefault(); // Evita que se envíe el formulario
-  
-    // Obtener los valores del formulario
+document.getElementById('agregarBtn').addEventListener('click', function() {
     var placa = document.getElementById('placa').value;
     var marca = document.getElementById('marca').value;
     var anio = document.getElementById('anio').value;
@@ -15,14 +12,12 @@ document.getElementById('myForm').addEventListener('submit', function(e) {
     var cargaGranelLiquida = document.getElementById('cargaGranelLiquida').checked;
     var cargasVarias = document.getElementById('cargasVarias').checked;
   
-    // Verificar si el campo de la placa está vacío y utilizar nduca o npiloza en su lugar
     if (placa === "") {
       var nduca = document.getElementById('nduca').value;
       var npiloza = document.getElementById('npiloza').value;
       placa = nduca || npiloza;
     }
   
-    // Crear una nueva fila en la tabla con los datos del formulario
     var table = document.getElementById('myTable');
     var row = table.insertRow(-1);
     var rowData = [placa, marca, anio, clase, tipo, cargasSecas, cargasPeligrosas, cargaPrederedera, cargaIndividual, cargaGranelSolida, cargaGranelLiquida, cargasVarias];
@@ -32,7 +27,6 @@ document.getElementById('myForm').addEventListener('submit', function(e) {
       cell.innerHTML = rowData[i];
     }
   
-    // Restablecer los valores del formulario
     document.getElementById('myForm').reset();
   });
   
